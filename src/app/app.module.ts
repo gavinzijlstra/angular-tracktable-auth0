348e7bcb04a1c836c97a06ef957460367eb1a82a
gavinzijlstra <gavinzijlstra@yahoo.com>
2019-10-09 14:59:14 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,47 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HighlightModule } from 'ngx-highlightjs';
+import json from 'highlight.js/lib/languages/json';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CallbackComponent } from './pages/callback/callback.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
+import { LoadingComponent } from './components/loading/loading.component';
+
+export function hljsLanguages() {
+  return [{ name: 'json', func: json }];
+}
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    CallbackComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavBarComponent,
+    FooterComponent,
+    HeroComponent,
+    HomeContentComponent,
+    LoadingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HighlightModule.forRoot({
+      languages: hljsLanguages
+    }),
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
